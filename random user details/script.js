e3678576c1d210928a5558ef5d0b6e86a3209b0d
NitroD <coderc013@gmail.com>
2026-02-22 08:40:57 +0000

    I finished work on the styling, scripting, and markup of the first of my API practices

---
+++ random user details/script.js
@@ -0,0 +1,32 @@
+
+const user = []
+let pictureFrame = document.getElementById('picture')
+let namePerson = document.getElementById('namePlaceholder')
+let jobtitle = document.getElementById('jobtitle')
+let email = document.getElementById('emailValue')
+let phone = document.getElementById('phoneValue')
+let company = document.getElementById('companyValue')
+let address = document.getElementById('addressValue')
+
+
+async function fetchRandomUser() {
+    try{
+        const response = await fetch('https://dummyjson.com/users?limit=100')
+        const data = await response.json()
+        user.push(...data.users)
+    }catch(err){
+        console.error('Error fetching data')
+    }
+    let random = Math.floor(Math.random() * user.length)
+    pictureFrame.innerHTML = `<img src="${user[random].image}" alt="profile picture">`
+    namePerson.innerText = `${user[random].firstName} ${user[random].lastName}`
+    jobtitle.innerText = `${user[random].company.title}`
+    email.innerText = `${user[random].email}`
+    phone.innerText = `${user[random].phone}`
+    company.innerText = `${user[random].company.name}`
+    address.innerText = `${user[random].address.address}, ${user[random].address.city}, ${user[random].address.state}, ${user[random].address.postalCode}`
+}
+
+
+
+fetchRandomUser();
